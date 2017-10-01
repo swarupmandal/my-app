5795b6aaa1fcf188133c094ab2b4c0a366c40bac
swarupmandal <swarup12x@gmail.com>
2017-10-02 01:00:01 +0530

    # Retrieve from db

--- src/main/java/org/hibernate/tutorial/app/EventManager.java
+++ src/main/java/org/hibernate/tutorial/app/EventManager.java
@@ -1,6 +1,7 @@
 package org.hibernate.tutorial.app;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.tutorial.domain.Event;
@@ -17,8 +18,14 @@ public class EventManager {
 
 		EventManager eventManager = new EventManager();
 		if (args[0].equals("store")) {
-			eventManager.createAndStoreEvent("Athletics", new Date());
+			eventManager.createAndStoreEvent("Cricket", new Date());
+			eventManager.createAndStoreEvent("Football", new Date());
 		}
+		
+		if (args[0].equals("list")) {
+			eventManager.listEvent();
+		}
+		
 		HibernateUtil.getSessionFactory().close();
 	}
 
@@ -35,6 +42,25 @@ public class EventManager {
 		session.getTransaction().commit();
 
 	}
+	
+	private List<Event> listEvent(){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		List<Event> events = session.createQuery("from Event").list();
+		
+		System.out.println("List Size >>> >> > " +events.size());
+		
+		for(Event e : events){
+			System.out.println("ID > " + e.getId() + " Title > " + e.getTitle() + " Date > " + e.getDate());
+		}
+		
+		session.getTransaction().commit();
+		
+		return events;
+		
+	}
+	
 	
 	
 
