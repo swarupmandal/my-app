2cd366c541b1f4cc33a00b777028977320dbfe15
swarup mandal <swarup12x@gmail.com>
2017-11-29 16:19:08 +0530

    abstract factory design pattern

--- designpattern/org/factorypattern/learn/ShapeFactory.java
+++ designpattern/org/factorypattern/learn/ShapeFactory.java
@@ -1,21 +1,22 @@
 package org.factorypattern.learn;
 
 import org.factorypattern.services.Shape;
+import org.factorypattern.services.impl.Circle;
+import org.factorypattern.services.impl.Rectangle;
+import org.factorypattern.services.impl.Square;
 
 public class ShapeFactory {
 
 	public Shape getShape(String shapeType) {
-		
-		if(shapeType == null) {
+
+		if (shapeType == null) {
 			return null;
 		}
-		if(shapeType.equals("CIRCLE")) {
+		if (shapeType.equals("CIRCLE")) {
 			return new Circle();
-		}
-		else if(shapeType.equals("RECTANGLE")) {
+		} else if (shapeType.equals("RECTANGLE")) {
 			return new Rectangle();
-		}
-		else if(shapeType.equals("SQUARE")) {
+		} else if (shapeType.equals("SQUARE")) {
 			return new Square();
 		}
 		return null;
--- designpattern/org/factorypattern/learn/Square.java
+++
@@ -1,9 +0,0 @@
-package org.factorypattern.learn;
-
-import org.factorypattern.services.Shape;
-
-public class Square implements Shape {
-	public void draw() {
-		System.out.println("SQUARE");
-	}
-}
